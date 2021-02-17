a8ed4e882614eed82e838a9f49fe249b2b01f59a
Diana Utan <didiutzi@gmail.com>
2021-02-17 02:25:09 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,10 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 html = open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML(html)
+doc.css(".headline-26OIBN").text
+# binding.pry
+p doc.css(".headline-26OIBN")[0].attribute
